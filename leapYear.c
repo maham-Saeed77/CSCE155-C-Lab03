4c5545ea93b147f9a92ea1f2631da9dcb272d0b2
Your Name <mahiqureshii77@gmail.com.unl.edu>
2023-10-22 00:51:21 +0500

    labb3

--- leapYear.c
+++ leapYear.c
@@ -1,53 +1,88 @@
+/**
+ * This program determines if various years are leap
+ * years or not.
+ *
+ */
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
 
+/**
+ * Returns true (1) if the given year is a leap year,
+ * false (0) if it is not a leap year.
+ */
 int isLeapYear(int year);
 
 int main(int argc, char **argv) {
-    bool reportPass = false;
-    if (argc > 1 && strcmp(argv[1], "-reportPass") == 0) {
-        reportPass = true;
-    }
 
-    int numPassed = 0;
-    int numFailed = 0;
+  bool reportPass = false;
+  if(argc > 1 && strcmp(argv[1], "-reportPass") == 0) {
+    reportPass = true;
+  }
 
-    // Test cases
-    int testCases[] = {2000, 2001, 2100, 2400, 2024, 1900};
+  int year;
+  int numPassed = 0;
+  int numFailed = 0;
 
-    for (int i = 0; i < sizeof(testCases) / sizeof(testCases[0]); i++) {
-        int year = testCases[i];
-        printf("Test Case %d: year = %d: ", i + 1, year);
+  //Hard-coded ad-hoc test cases
+  //Do not change these, add your own test cases
+  //below.  All test cases should pass.
 
-        if (isLeapYear(year)) {
-            printf("PASSED!\n");
-            numPassed++;
-        } else {
-            printf("FAILED!\n");
-            numFailed++;
-        }
-    }
+  year = 2000;
+  printf("Test Case 1: year = %d: ", year);
+  if(!isLeapYear(year)) {
+    printf("FAILED!\n");
+    numFailed = numFailed + 1;
+  } else {
+    printf("PASSED!\n");
+    numPassed = numPassed + 1;
+  }
 
-    printf("\n\n");
-    printf("Summary:\n");
-    printf("Number of test cases passed: %d\n", numPassed);
-    printf("Number of test cases failed: %d\n", numFailed);
-    printf("Percentage Passed: %.2f%%\n", (double)numPassed / (numPassed + numFailed) * 100.0);
+  year = 2001;
+  printf("Test Case 2: year = %d: ", year);
+  if(isLeapYear(year)) {
+    printf("FAILED!\n");
+    numFailed = numFailed + 1;
+  } else {
+    printf("PASSED!\n");
+    numPassed = numPassed + 1;
+  }
 
-    if (reportPass) {
-        return numPassed;
-    } else {
-        return numFailed;
-    }
+  year = 2100;
+  printf("Test Case 3: year = %d: ", year);
+  if(isLeapYear(year)) {
+    printf("FAILED!\n");
+    numFailed = numFailed + 1;
+  } else {
+    printf("PASSED!\n");
+    numPassed = numPassed + 1;
+  }
+
+  //TODO: write at least 3 more of your own
+  //      test cases here, they should all pass!
+
+  printf("\n\n");
+  printf("Summary:\n");
+  printf("Number of test cases passed: %d\n", numPassed);
+  printf("Number of test cases failed: %d\n", numFailed);
+  printf("Percentage Passed: %.2f%%\n", (double) numPassed / (numPassed + numFailed) * 100.0);
+
+  if(reportPass) {
+    return numPassed;
+  } else {
+    return numFailed;
+  }
 }
 
 int isLeapYear(int year) {
-    if (year % 4 == 0) {
-        if (year % 100 != 0 || year % 400 == 0) {
-            return 1;  // It's a leap year
-        }
-    }
-    return 0;  // It's not a leap year
+if (year%4== &&year%400==0 || year%100==0)
+{
+  return 1;
+}
+else
+{
+  return 0;
+}
+return 0;
 }
