1a715496bdab41d1131c044c7e48a9fecbabf8f3
Your Name <mahiqureshii77@gmail.com.unl.edu>
2023-10-22 01:18:18 +0500

    lab3p3

--- taxes.c
+++ taxes.c
@@ -29,6 +29,23 @@ int main(int argc, char **argv) {
   }
 
   //TODO: compute the tax, child credit, and total tax here
+  if(agi>0 && agi<=500)
+  {
+    tax = agi * 0.10;
+    printf ("%f, tax");
+  }
+  else if (agi>50000 && agi<=100000)
+  {
+    tax = 5000+(agi-50000) * 0.20;
+    printf ("%f, tax");
+  }
+  else
+  {
+    printf ("tax");
+  }
+
+  childCredit = numChildern * 1000;
+  totalTax = childCredit - tax;
 
   printf("AGI:          $%10.2f\n", agi);
   printf("Tax:          $%10.2f\n", tax);
